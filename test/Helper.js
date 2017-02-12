bf75af3d27c834da8b171218971c0100c2e4c2c4
zoltan-mihalyi <zoltan.mihalyi@webvalto.hu>
2017-02-12 23:00:30 +0100

    node 0.x support

--- test/Helper.js
+++ test/Helper.js
@@ -4,5 +4,5 @@ var assert = require('assert');
 
 module.exports.assertMatch = function ensureMatch(filename) {
     var expected = fs.readFileSync(__dirname + '/samples/' + filename + '.optimized.js', 'UTF-8').replace(/\r/g, '');
-    assert.equal(optimize(fs.readFileSync(`${__dirname}/samples/${filename}.js`, 'UTF-8')), expected);
+    assert.equal(optimize(fs.readFileSync(__dirname + '/samples/' + filename + '.js', 'UTF-8')), expected);
 };
