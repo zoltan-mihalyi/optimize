80748eb0f837cb9a99d5a35fff726a3bb845451e
zoltan-mihalyi <zoltan.mihalyi@webvalto.hu>
2017-02-12 11:25:33 +0100

    test for parameter variables are initialized

--- test/SemanticNodeTest.js
+++ test/SemanticNodeTest.js
@@ -134,4 +134,11 @@ describe('Semantic node test', function() {
         assert.strictEqual(varI.reads.length, 0);
         assert.strictEqual(varI.writes.length, 1);
     });
+
+    it('function parameters are initialized', function() {
+        var ast = recast.parse('function fn(a){} (function(a){})').program;
+        var semanticNode = node.semantic(ast);
+        assert(semanticNode.body[0].body.scope.get('a').initialized);
+        assert(semanticNode.body[1].expression.body.scope.get('a').initialized);
+    });
 });
